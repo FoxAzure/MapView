e21d9cc71ba70e8b355f778d33a62822be61d5bf
FoxAzure <108664801+FoxAzure@users.noreply.github.com>
2023-06-02 08:05:15 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -312,7 +312,7 @@ function recarregarpagina(){
     var cod = localStorage.getItem("cod");
     var campo = localStorage.getItem("campo");
 
-    setTimeout(location.reload(), 50000);
+    setTimeout(location.reload(), 150000);
     //setTimeout(alert(`Planta ${cod} - ${campo} baixa`), 8000);
     //alert(`Planta ${cod} - ${campo} baixada com sucesso`)
 }
